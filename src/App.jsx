7d279097e86f799b63b96f133329791a9adb7037
Özge Ahras <ozgeahras@gmail.com>
2023-05-09 15:23:06 +0300

    added react-confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,91 @@
+import { useState } from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import { useEffect } from "react";
+import Confetti from "react-confetti";
 
 export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [dice]);
+
+  /**
+   * Challenge: Tie off loose ends!
+   * 1. If tenzies is true, Change the button text to "New Game"
+   * 2. If tenzies is true, use the "react-confetti" package to
+   *    render the <Confetti /> component 🎉
+   *
+   *    Hint: don't worry about the `height` and `width` props
+   *    it mentions in the documentation.
+   */
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const numberArray = [];
+    for (var i = 0; i < 10; i++) {
+      numberArray.push(generateNewDie());
+    }
+    return numberArray;
+  }
+
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die, index) => (
+    <Die
+      value={die.value}
+      key={index}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="0" />
-        <Die value="1" />
-      </div>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+      <Confetti
+        width={500}
+        height={500}
+        numberOfPieces={1000}
+        recycle={false}
+      />
     </main>
   );
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles} onClick={props.holdDice}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
